553d763870df969dcca4945b058ac49c838cc4fa
Kunal <virajprajapat854@gmail.com>
2025-04-05 19:10:57 +0530

    intial commit

---
+++ src/in/kunal/Sum_Of_N_Numbers.java
@@ -0,0 +1,18 @@
+package in.kunal;
+
+
+public class Sum_Of_N_Numbers {
+	
+	public static void main(String[] args) {
+		int num = 10;
+//		int sum = 0;
+//		for(int i = 1 ; i<=num;i++) {
+//			sum +=i;
+//			System.out.println(sum);
+//		}
+		
+		num = num * (num+1)/2;
+		System.out.println(num);  
+	}
+
+}
